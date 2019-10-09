bc09d6a047f1f30a57658eabca87ba2554d1798e
Ganeshk750 <ganesh9ot@gmail.com>
2019-10-09 22:27:20 +0530

    first final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { ModalComponent } from './modal/modal.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ModalComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
