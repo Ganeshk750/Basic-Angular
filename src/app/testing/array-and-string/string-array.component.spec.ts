199adc41cda9faa399ab4de6d4ee9ec76666f12e
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-10 16:50:50 +0530

    # adding array function and testing these array function

--- src/app/testing/array-and-string/string-array.component.spec.ts
+++ src/app/testing/array-and-string/string-array.component.spec.ts
@@ -1,8 +1,19 @@
-import { greet } from './string-array.component';
+import { greet, getCurrencies } from './string-array.component';
 
 describe('greet', () => {
     it('Should include the name in the message', () => {
         // expect(greet('Ganesh')).toBe('Welcome Ganesh');
         expect(greet('Prince')).toContain('Prince');
+    })
+})
+
+
+describe('getCurrencies', () => {
+    it('Should return the supported Currencies', () => {
+        const result = getCurrencies();
+        expect(result).toContain('INR');
+        expect(result).toContain('USD');
+        expect(result).toContain('URO');
+        expect(result).toContain('AUD');
     })
 })
--- src/app/testing/array-and-string/string-array.component.ts
+++ src/app/testing/array-and-string/string-array.component.ts
@@ -1,3 +1,7 @@
 export function greet(name) {
     return 'Welcome ' + name;
 }
+
+export function getCurrencies() {
+    return ['USD', 'URO', 'AUD', 'INR']
+}
