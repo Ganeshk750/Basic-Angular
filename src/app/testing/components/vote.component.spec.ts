5fd7d97632df964c6c3efe0fba68930402be97d6
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-10 17:27:34 +0530

    # code refactring

--- src/app/testing/components/vote.component.spec.ts
+++ src/app/testing/components/vote.component.spec.ts
@@ -13,3 +13,27 @@ describe('VoteComponent', () => {
         expect(component.totalVotes).toBe(-1);
     })
 })
+
+//code-refactoring
+
+/* describe('VoteComponent', () => {
+    let component: VoteComponent;
+    beforeEach(() => {
+        component = new VoteComponent();
+        it('==>Should increment totalVotes When upVoted', () => {
+            let component = new VoteComponent();
+            component.upVote();
+            expect(component.totalVotes).toBe(1);
+        })
+    })
+
+    afterEach(() => {
+        it('==>Should decerment totalVotes When downVoted', () => {
+            let component = new VoteComponent();
+            component.downVote();
+            expect(component.totalVotes).toBe(-1);
+        })
+    })
+})
+
+ */
