fe757bd946929f0b29916801dfb22d9e24769fb5
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-13 19:49:00 +0530

    # removing un-used code

--- src/app/testing/components/vote.component.spec.ts
+++ src/app/testing/components/vote.component.spec.ts
@@ -1,6 +1,6 @@
 import { VoteComponent } from './vote.component';
 
-describe('VoteComponent', () => {
+ describe('VoteComponent', () => {
     it('Should increment totalVotes When upVoted', () => {
         let component = new VoteComponent();
         component.upVote();
@@ -12,28 +12,8 @@ describe('VoteComponent', () => {
         component.downVote();
         expect(component.totalVotes).toBe(-1);
     })
-})
+}) 
 
-//code-refactoring
 
-/* describe('VoteComponent', () => {
-    let component: VoteComponent;
-    beforeEach(() => {
-        component = new VoteComponent();
-        it('==>Should increment totalVotes When upVoted', () => {
-            let component = new VoteComponent();
-            component.upVote();
-            expect(component.totalVotes).toBe(1);
-        })
-    })
 
-    afterEach(() => {
-        it('==>Should decerment totalVotes When downVoted', () => {
-            let component = new VoteComponent();
-            component.downVote();
-            expect(component.totalVotes).toBe(-1);
-        })
-    })
-})
-
- */
+ 
