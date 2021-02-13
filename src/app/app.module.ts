1bc0d2d0b6de742678eb7712b0d6b9bd580ef34f
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-13 20:19:16 +0530

    # adding event-emmiter folder with vote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoFormComponent } from './testing/todo-form/todo-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VoteComponent } from './testing/event-emmiters/vote/vote.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoFormComponent
+    TodoFormComponent,
+    VoteComponent
   ],
   imports: [
     BrowserModule,
