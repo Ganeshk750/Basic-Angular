cddf417ec8a6dd953f5fade780ac725815953828
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-13 19:53:38 +0530

    # importing FormsModule and ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoFormComponent } from './testing/todo-form/todo-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,6 +15,8 @@ import { TodoFormComponent } from './testing/todo-form/todo-form.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
