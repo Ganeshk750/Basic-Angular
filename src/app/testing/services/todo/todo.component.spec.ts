e4ec59a2dd5abdc96a6d2dd581b0af308656b21a
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-14 20:16:04 +0530

    adding simple todo test

--- src/app/testing/services/todo/todo.component.spec.ts
+++ src/app/testing/services/todo/todo.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoComponent } from './todo.component';
+import { TodoService } from './todo.service';
+import { Observable } from 'rxjs';
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
+  let service: TodoService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
+      declarations: [ TodoComponent ],
+      providers: [TodoService]
     })
     .compileComponents();
   }));
@@ -16,10 +20,22 @@ describe('TodoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
+    service = new TodoService(null);
+    component = new TodoComponent(service);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should set todo property with the items returned from the service', () =>{
+    let todos = [1,2,3];
+    spyOn(service, 'getTodos').and.callFake(() =>{
+      return Observable.arguments([todos])
+    });
+    component.ngOnInit();
+    expect(component.todos.length).toBeGreaterThan(0);
+  })
+
 });
