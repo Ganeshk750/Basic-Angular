11083a7e535b4d69a88a88078ed2def0cc3f3afb
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-14 19:36:45 +0530

    adding service add and delete method using http

--- src/app/testing/services/todo/todo.service.ts
+++ src/app/testing/services/todo/todo.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  add(todo) {
+    return this.http.post('...', todo).pipe(
+      map(r => r)
+    );
+  }
+
+  getTodos() {
+    return this.http.get('...').pipe(
+      map(r => r)
+    );
+  }
+
+  delete(id) {
+    return this.http.delete('...').pipe(
+      map(r => r)
+    );
+  }
 }
